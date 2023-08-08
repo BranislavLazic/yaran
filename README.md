642ba12af96dab04119b255c9ba76d6ef1ba8687
Branislav Lazic <brano2411@hotmail.com>
2023-08-08 20:28:09 +0200

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Yaran
 
 A Lisp parser
+
+## How to use
+
+Run `dune build` and then parse an expression. E.g.: `./_build/install/default/bin/yaran "(+ 1 3)"`
--- lib/ast.ml
+++ lib/ast.ml
@@ -5,8 +5,7 @@ type sexp = Atom of atom | ListSexp of sexp list [@@deriving show]
 type statement = Statement of sexp list [@@deriving show]
 
 (* Flatten a list of S-Expressions or return a single atom if the list has an only one element *)
-let flatten s =
-  match s with
+let flatten = function
   | ListSexp [ ListSexp sexp ] -> ListSexp sexp
   | ListSexp s when List.length s == 1 -> List.hd s
   | s -> s
